87f467aa12f687e16a20a6fe924c868668ca5276
doconnor <doconnor@numerx.com>
2022-05-22 19:01:50 +0100

    fix curl in dockekr test

--- build/run-test.sh
+++ build/run-test.sh
@@ -1,9 +1,9 @@
 code=`curl -o /dev/null -s -w "%{http_code}\n" http://supervillain:8080/index.html`
 if [ $code == "200" ] 
 then
-    echo "curl success: supervillain: /index.html.html page"
+    echo "curl success: supervillain: /index.html page"
 else
-    echo "curl fail: supervillain: /index.html.html page"
+    echo "curl fail: supervillain: /index.html page"
     exit 1
 fi
 
